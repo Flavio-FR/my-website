e38769e1f651d18df1a6f3067ff95ac9fb1e674a
DocuGhost <1554540@senati.pe>
2025-04-23 07:03:20 -0500

    feat: Agrego carpeta src con componentes personalizados

---
+++ src/pages/index.js
@@ -0,0 +1,46 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import gifAnimado from '@site/static/img/fantasmin.gif';
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header 
+    className={clsx('hero hero--primary', styles.heroBanner)}
+    style={{ paddingBottom: '2rem', paddingTop: '2rem' }} // Ajusta los valores de padding
+    >
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <img
+            src={gifAnimado}
+            alt="Fantasma animado"
+            style={{ width: '150px', height: 'auto' }} // Ajusta el ancho al tamaño deseado
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
